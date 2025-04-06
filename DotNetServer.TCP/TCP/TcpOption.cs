a569b74ee660e921d7b07ad5d4ff72f1a8b6b5d4
M.Z. <zeynalov@gmail.com>
2025-04-06 19:48:04 +0400

    drafted tcp options...

---
+++ DotNetServer.TCP/TCP/TcpOption.cs
@@ -0,0 +1,42 @@
+﻿namespace DotNetServer.TCP.TCP;
+public abstract class TcpOption
+{
+    protected TcpOption(TcpOptionsKind kind, int length)
+    {
+        Kind = kind;
+        Length = length;
+    }
+
+    public TcpOptionsKind Kind { get; }
+    public int Length { get; }
+}
+
+public class TcpOptionNone : TcpOption
+{
+    public TcpOptionNone() : base(TcpOptionsKind.EndOfOptionsList, 1) {}
+}
+
+public class TcpOptionNoOp : TcpOption
+{
+    public TcpOptionNoOp() : base(TcpOptionsKind.NoOp, 1) { }
+}
+
+public class TcpOptionMss : TcpOption
+{
+    public TcpOptionMss() : base(TcpOptionsKind.MaximumSegmentSize, 4)
+    {
+        MaximumSegmentSize = 4;
+    }
+
+    public ushort MaximumSegmentSize { get; }
+}
+
+public class TcpOptionWindowScale : TcpOption
+{
+    public TcpOptionWindowScale() : base(TcpOptionsKind.MaximumSegmentSize, 3)
+    {
+        WindowScale = 3;
+    }
+
+    public byte WindowScale { get; }
+}
