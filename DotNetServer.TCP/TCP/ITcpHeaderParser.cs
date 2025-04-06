bde69a42c878287963c2403cb6088bbbb425715d
M.Z. <zeynalov@gmail.com>
2025-04-06 12:36:43 +0400

    parser finalized (not tested), added to string method to tcp header.

--- DotNetServer.TCP/TCP/ITcpHeaderParser.cs
+++ DotNetServer.TCP/TCP/ITcpHeaderParser.cs
@@ -12,7 +12,29 @@ public class TcpHeaderParser : ITcpHeaderParser
 {
     public TcpHeader Decode(byte[] data, int startIndex, out int length)
     {
+        if (data is null or [] || data.Length < startIndex + 20)
+            throw new ArgumentException("Invalid TCP header.");
 
+        var sourcePort = data[startIndex] << 8 | data[startIndex + 1];
+        var destinationPort = data[startIndex+2] << 8 | data[startIndex + 3];
+
+        var sequenceNumber = BitConverter.ToInt32(data, startIndex + 4);
+        var acknowledgementNumber = BitConverter.ToInt32(data, startIndex + 8);
+
+        var dataOffset = (byte)(data[startIndex + 12] >> 4);
+        var flags = (TcpHeaderFlags)data[startIndex + 13];
+
+        var window = data[startIndex + 14] << 8 | data[startIndex + 15];
+        var checkSum = data[startIndex + 16] << 8 | data[startIndex + 17];
+        var urgentPointer = data[startIndex + 18] << 8 | data[startIndex + 19];
+
+        var header = new TcpHeader(sourcePort, destinationPort, sequenceNumber, acknowledgementNumber,
+            dataOffset, flags, window, checkSum, urgentPointer);
+        length = header.TcpHeaderLength;
+
+        return header;
     }
-    public void Encode(TcpHeader ipHeader, byte[] data, int startIndex, out int length) => throw new NotImplementedException();
+
+    public void Encode(TcpHeader ipHeader, byte[] data, int startIndex, out int length)
+        => throw new NotImplementedException();
 }
--- DotNetServer.TCP/TCP/TcpHeader.cs
+++ DotNetServer.TCP/TCP/TcpHeader.cs
@@ -1,6 +1,30 @@
-﻿namespace DotNetServer.TCP.TCP;
+﻿using System.Text;
+
+namespace DotNetServer.TCP.TCP;
 public class TcpHeader
 {
+    public TcpHeader(
+        int sourcePort,
+        int destinationPort,
+        int sequenceNumber,
+        int acknowledgementNumber,
+        byte dataOffset,
+        TcpHeaderFlags flags,
+        int window,
+        int checksum,
+        int urgentPointer)
+    {
+        SourcePort = sourcePort;
+        DestinationPort = destinationPort;
+        SequenceNumber = sequenceNumber;
+        AcknowledgementNumber = acknowledgementNumber;
+        DataOffset = dataOffset;
+        Flags = flags;
+        Window = window;
+        Checksum = checksum;
+        UrgentPointer = urgentPointer;
+    }
+
     public int SourcePort { get; }
     public int DestinationPort { get; }
     public int SequenceNumber { get; }
@@ -12,6 +36,25 @@ public class TcpHeader
     public int Checksum { get; }
     public int UrgentPointer { get; }
     //placeholder property to sort out options - for now skipping. 
-    public int OptionsLength { get; }
+    public int OptionsLength { get => TcpHeaderLength - 20; }
 
+    public override string ToString()
+    {
+        var builder = new StringBuilder();
+        builder.AppendLine("------- TCP Header Start --------");
+        builder.AppendLine($"SourcePort: {SourcePort}");
+        builder.AppendLine($"DestinationPort: {DestinationPort}");
+        builder.AppendLine($"SequenceNumber: {SequenceNumber}");
+        builder.AppendLine($"AcknowledgementNumber: {AcknowledgementNumber}");
+        builder.AppendLine($"DataOffset: {DataOffset}");
+        builder.AppendLine($"TcpHeaderLength: {TcpHeaderLength}");
+        builder.AppendLine($"Flags: {Flags}");
+        builder.AppendLine($"Window: {Window}");
+        builder.AppendLine($"Checksum: {Checksum}");
+        builder.AppendLine($"UrgentPointer: {UrgentPointer}");
+        builder.AppendLine($"OptionsLength: {OptionsLength}");
+        builder.AppendLine("------- TCP Header End --------");
+
+        return builder.ToString();
+    }
 }
