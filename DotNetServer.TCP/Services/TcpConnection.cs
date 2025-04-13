cdbfd663328d6fa24da9905c029f1ad3259ff4eb
M.Z. <zeynalov@gmail.com>
2025-04-13 17:06:07 +0400

    changed properties to readonly fields.

--- DotNetServer.TCP/Services/TcpConnection.cs
+++ DotNetServer.TCP/Services/TcpConnection.cs
@@ -2,7 +2,7 @@
 
 //for now this service assumed one packet per request, later will be extended...
 // check if it needs to be disposable
-public class TcpConnection 
+public class TcpConnection
 {
     public TcpConnection(
         ITcpConnectionManager connectionManager,
@@ -16,31 +16,34 @@ public class TcpConnection
         uint? timestampEchoReply,
         ushort? timeoutInMs)
     {
-        Key = key;
-        State = state;
-        MaximumSegmentSize = maximumSegmentSize;
-        WindowScale = windowScale;
-        SackPermitted = sackPermitted;
-        SackBlocks = sackBlocks;
-        TimestampValue = timestampValue;
-        TimestampEchoReply = timestampEchoReply;
-        TimeoutInMs = timeoutInMs;
+        _key = key;
+        _state = state;
+        _maximumSegmentSize = maximumSegmentSize;
+        _windowScale = windowScale;
+        _sackPermitted = sackPermitted;
+        _sackBlocks = sackBlocks;
+        _timestampValue = timestampValue;
+        _timestampEchoReply = timestampEchoReply;
+        _timeoutInMs = timeoutInMs;
         _connectionManager = connectionManager;
     }
 
-    public TcpConnectionKey Key { get; }
-    public TcpConnectionState State { get; }
-    public ushort? MaximumSegmentSize { get; }
-    public byte? WindowScale { get; }
-    public bool? SackPermitted { get; }
-    public List<(uint, uint)> SackBlocks { get; }
-    public uint? TimestampValue { get; }
-    public uint? TimestampEchoReply { get; }
-    public ushort? TimeoutInMs { get; }
+    private readonly TcpConnectionKey _key;
+    private TcpConnectionState _state;
+    private ushort? _maximumSegmentSize;
+    private byte? _windowScale;
+    private bool? _sackPermitted;
+    private List<(uint, uint)> _sackBlocks;
+    private uint? _timestampValue;
+    private uint? _timestampEchoReply;
+    private ushort? _timeoutInMs;
     private readonly ITcpConnectionManager _connectionManager;
 
-    public async Task<(HttpData httpData, bool shouldReturn, bool shouldDropConnection)> HandleRequest(TcpData tcpData) =>
-        throw new NotImplementedException();
+    public async Task<(HttpData httpData, bool shouldReturn, bool shouldDropConnection)> HandleRequest(
+        TcpData tcpData)
+    {
+
+    }
 
     public async Task<TcpData> Send(HttpData httpData) => throw new NotImplementedException(); 
 }
