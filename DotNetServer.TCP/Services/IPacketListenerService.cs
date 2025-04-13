828cd150ffd5839d9243ffab078eb3c562c59f82
M.Z. <zeynalov@gmail.com>
2025-04-13 08:18:02 +0400

    drafted the subscribe method.

--- DotNetServer.TCP/Services/IPacketListenerService.cs
+++ DotNetServer.TCP/Services/IPacketListenerService.cs
@@ -1,4 +1,6 @@
 ﻿using System.Net;
+using System.Net.Sockets;
+using System.Runtime.CompilerServices;
 
 namespace DotNetServer.TCP.Services;
 
@@ -10,4 +12,27 @@ public interface IPacketListenerService
     IAsyncEnumerable<BufferData> Subscribe(IPAddress ipAddressToListen,
         int portToListen, CancellationToken cancellationToken);
     Task SendAsync(byte[] dataToSend, IPAddress destinationIpAddress, int destinationPort); 
+}
+
+public class PacketListenerService : IPacketListenerService
+{
+    public async IAsyncEnumerable<BufferData> Subscribe(IPAddress ipAddressToListen, int portToListen,
+        [EnumeratorCancellation]CancellationToken cancellationToken)
+    {
+        using var rawSocket = new Socket(AddressFamily.InterNetwork, SocketType.Raw, ProtocolType.IP);
+        rawSocket.Bind(new IPEndPoint(ipAddressToListen, portToListen));
+        rawSocket.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.HeaderIncluded, true);
+        byte[] buffer = new byte[65535];
+
+        while (!cancellationToken.IsCancellationRequested)
+        {
+            int bytesRead = await rawSocket.ReceiveAsync(buffer);
+
+            var bufferData = new BufferData(buffer, bytesRead);
+
+            yield return bufferData; 
+        }
+    }
+
+    public Task SendAsync(byte[] dataToSend, IPAddress destinationIpAddress, int destinationPort) => throw new NotImplementedException();
 }
