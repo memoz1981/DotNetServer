ad5534c9624cbd186a8d36796fe2eeb0987082ee
M.Z. <zeynalov@gmail.com>
2025-04-13 08:33:02 +0400

    drafted both methods, made disposable...

--- DotNetServer.TCP/Services/IPacketListenerService.cs
+++ DotNetServer.TCP/Services/IPacketListenerService.cs
@@ -7,26 +7,32 @@ namespace DotNetServer.TCP.Services;
 /// <summary>
 /// Service to send/receive byte array data (length is important to specify, especially while listening - as buffer will be used)
 /// </summary>
-public interface IPacketListenerService
+public interface IPacketListenerService : IDisposable
 {
     IAsyncEnumerable<BufferData> Subscribe(IPAddress ipAddressToListen,
         int portToListen, CancellationToken cancellationToken);
-    Task SendAsync(byte[] dataToSend, IPAddress destinationIpAddress, int destinationPort); 
+    Task SendAsync(byte[] dataToSend, IPEndPoint endPoint); 
 }
 
 public class PacketListenerService : IPacketListenerService
 {
+    private Socket _dataSocket = null;
+
     public async IAsyncEnumerable<BufferData> Subscribe(IPAddress ipAddressToListen, int portToListen,
         [EnumeratorCancellation]CancellationToken cancellationToken)
     {
-        using var rawSocket = new Socket(AddressFamily.InterNetwork, SocketType.Raw, ProtocolType.IP);
-        rawSocket.Bind(new IPEndPoint(ipAddressToListen, portToListen));
-        rawSocket.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.HeaderIncluded, true);
+        if (_dataSocket is not null)
+            throw new InvalidOperationException("Already listening to another stream...");
+
+        _dataSocket = new Socket(AddressFamily.InterNetwork, SocketType.Raw, ProtocolType.IP);
+        _dataSocket.Bind(new IPEndPoint(ipAddressToListen, portToListen));
+        _dataSocket.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.HeaderIncluded, true);
+
         byte[] buffer = new byte[65535];
 
         while (!cancellationToken.IsCancellationRequested)
         {
-            int bytesRead = await rawSocket.ReceiveAsync(buffer);
+            int bytesRead = await _dataSocket.ReceiveAsync(buffer);
 
             var bufferData = new BufferData(buffer, bytesRead);
 
@@ -34,5 +40,8 @@ public class PacketListenerService : IPacketListenerService
         }
     }
 
-    public Task SendAsync(byte[] dataToSend, IPAddress destinationIpAddress, int destinationPort) => throw new NotImplementedException();
+    public void Dispose() => _dataSocket?.Dispose();
+
+    public async Task SendAsync(byte[] dataToSend, IPEndPoint endPoint)
+        => await _dataSocket.SendToAsync(dataToSend, endPoint); 
 }
